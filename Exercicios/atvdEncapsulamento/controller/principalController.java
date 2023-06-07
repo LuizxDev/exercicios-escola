5b5ae50dd28d4ec75b8a28d7cb7ff6b76e1ca805
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-07 01:35:26 -0300

    codigo refatorado

--- Exercicios/atvdEncapsulamento/controller/principalController.java
+++ Exercicios/atvdEncapsulamento/controller/principalController.java
@@ -4,16 +4,16 @@ import Exercicios.atvdEncapsulamento.model.Carro;
 import Exercicios.atvdEncapsulamento.model.Pessoa;
 
 public class principalController {
-    
-    public void cadastroPessoa(String nome, int idade){
+
+    public void cadastroPessoa(String nome, int idade) {
 
         Pessoa pessoa = new Pessoa();
-        
 
         pessoa.setNome(nome);
         pessoa.setIdade(idade);
- 
+
     }
+
     public void cadastroCarro(String marca, String modelo, int ano) {
         Carro carro = new Carro();
 
--- Exercicios/atvdEncapsulamento/view/Aplicacao.java
+++ Exercicios/atvdEncapsulamento/view/Aplicacao.java
@@ -19,33 +19,31 @@ public class Aplicacao {
 
         principalController controller = new principalController();
 
-    String nome = JOptionPane.showInputDialog(null,"Ola, Digite seu nome");
+        String nome = JOptionPane.showInputDialog(null, "Ola, Digite seu nome");
 
-    int idade = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite sua Idade"));
+        int idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite sua Idade"));
 
-    controller.cadastroPessoa(nome, idade);
+        controller.cadastroPessoa(nome, idade);
 
-    if(idade < 18){
-        JOptionPane.showMessageDialog(null, "Não é possível registrar um carro, pois você não possui idade suficiente.");
-    }else{
-        String marca = JOptionPane.showInputDialog(null,"Digite a marca do Carro");
+        if (idade < 18) {
+            JOptionPane.showMessageDialog(null,
+                    "Não é possível registrar um carro, pois você não possui idade suficiente.");
+        } else {
+            String marca = JOptionPane.showInputDialog(null, "Digite a marca do Carro");
 
-        String modelo = JOptionPane.showInputDialog(null,"Digite o modelo do Carro");
+            String modelo = JOptionPane.showInputDialog(null, "Digite o modelo do Carro");
 
-        int ano = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o ano do Carro"));
+            int ano = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o ano do Carro"));
 
-        controller.cadastroCarro(marca, modelo, ano);
+            controller.cadastroCarro(marca, modelo, ano);
 
-        JOptionPane.showMessageDialog(null, "Registro feito com sucesso!");
+            JOptionPane.showMessageDialog(null, "Registro feito com sucesso!");
 
-        
+            JOptionPane.showMessageDialog(null, "Seus dados: \nNome: " + nome + "\nIdade: " + idade
+                    + "\nDados do Carro \nMarca: " + marca + "\nModelo:" + modelo + "\nAno: " + ano);
 
-
+        }
 
     }
-
-    }
-
-
 
 }
