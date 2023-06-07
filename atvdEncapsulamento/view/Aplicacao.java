b1030bbbdd66b0a48932f8382b7e3bd550c6f1bf
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-07 00:36:44 -0300

    tags de versionamento

--- atvdEncapsulamento/view/Aplicacao.java
+++ atvdEncapsulamento/view/Aplicacao.java
@@ -1,5 +1,14 @@
 package atvdEncapsulamento.view;
 
+/**
+ * Descrição da classe.
+ *
+ * @author Luiz Felipe
+ * @version 1.0
+ * @since 07/06/2023
+ * @see <a href="https://github.com/LuizxDev">Link dos meus repositórios</a>
+ */
+
 public class Aplicacao {
     
 }
