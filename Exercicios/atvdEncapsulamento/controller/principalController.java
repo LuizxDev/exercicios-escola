29c08986a8a28494ea13bda45c0eed0547ed1193
LuizxDev <luizxfelipe.dev@gmail.com>
2023-06-07 01:26:14 -0300

    funcao principal
    funçao principal concluida

--- Exercicios/atvdEncapsulamento/controller/principalController.java
+++ Exercicios/atvdEncapsulamento/controller/principalController.java
@@ -8,15 +8,18 @@ public class principalController {
     public void cadastroPessoa(String nome, int idade){
 
         Pessoa pessoa = new Pessoa();
-        Carro carro = new Carro();
+        
 
         pessoa.setNome(nome);
         pessoa.setIdade(idade);
  
     }
-
     public void cadastroCarro(String marca, String modelo, int ano) {
-        
+        Carro carro = new Carro();
+
+        carro.setMarca(modelo);
+        carro.setModelo(modelo);
+        carro.setAno(ano);
     }
 
 }
--- Exercicios/atvdEncapsulamento/view/Aplicacao.java
+++ Exercicios/atvdEncapsulamento/view/Aplicacao.java
@@ -28,6 +28,19 @@ public class Aplicacao {
     if(idade < 18){
         JOptionPane.showMessageDialog(null, "Não é possível registrar um carro, pois você não possui idade suficiente.");
     }else{
+        String marca = JOptionPane.showInputDialog(null,"Digite a marca do Carro");
+
+        String modelo = JOptionPane.showInputDialog(null,"Digite o modelo do Carro");
+
+        int ano = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o ano do Carro"));
+
+        controller.cadastroCarro(marca, modelo, ano);
+
+        JOptionPane.showMessageDialog(null, "Registro feito com sucesso!");
+
+        
+
+
 
     }
 
